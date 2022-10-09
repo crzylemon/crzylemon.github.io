1606780c412d14828a501aaf1f2d8ced488a383a
emmettdewet14 <115381147+emmettdewet14@users.noreply.github.com>
2022-10-09 19:17:59 -0400

    sususususuususususus

--- Emmettblocks.js
+++ Emmettblocks.js
@@ -1,55 +1,49 @@
 class Emmettblocks {
-    constructor() {
-    }
-    
-    getInfo() {
-        return {
-            "id": "emmettblocks",
-            "name": "Emmettblocks",
-                        "blocks": [
-                        {
-                            "opcode": "whengiven",
-                            "blockType": "hat",
-                            "text": "When [thing] recived from cloud",
-                            "arguments": {
-                                "thing": {
-                                    "type": "string",
-                                    "defaultValue": "message1"
-                                },
-                            }
-                        },
-                        {
-                            "opcode": "give",
-                            "blockType": "command",
-                            "text": "Give string [brod]",
-                            "arguments": {
-                                "brod": {
-                                    "type": "string",
-                                    "defaultValue": "message1"
-                                },
-                            }
-                        },
-                          {
-                            "opcode": "resetall",
-                            "blockType": "command",
-                            "text": "Reset all",
-                            "arguments": {},
-                            }
-                        },
-                ]
-        }
-    }
-    
-        whengiven({thing}) {
-        
-    }
-    
-    give({brod}) {
-        return "Gave " + brod + " to cloud!"
-    }
-    resetall({}) {
-        return "Reset cloud data!"
-    }
+	constructor() {}
+
+	getInfo() {
+		return {
+			"id": "emmettblocks",
+			"name": "Emmettblocks",
+			"blocks": [{
+				"opcode": "whengiven",
+				"blockType": "hat",
+				"text": "When [thing] recived from cloud",
+				"arguments": {
+					"thing": {
+						"type": "string",
+						"defaultValue": "message1"
+					},
+				}
+			}, {
+				"opcode": "give",
+				"blockType": "command",
+				"text": "Give string [brod]",
+				"arguments": {
+					"brod": {
+						"type": "string",
+						"defaultValue": "message1"
+					},
+				}
+			}, {
+				"opcode": "resetall",
+				"blockType": "command",
+				"text": "Reset all",
+				"arguments": {},
+			}]
+		};
+	}
+
+	whengiven({thing}) {
+
+	}
+
+	give({brod}) {
+		return "Gave " + brod + " to cloud!";
+	}
+	resetall({}) {
+		return "Reset cloud data!";
+	}
 }
 
-Scratch.extensions.register(new Emmettblocks())
+Scratch.extensions.register(new Emmettblocks());
