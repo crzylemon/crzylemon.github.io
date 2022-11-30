b6a9789ecbe3c89e5a907dfdf8c75c1263501864
Crzymnkyboy <115381147+emmettdewet14@users.noreply.github.com>
2022-11-29 22:28:23 -0500

    Create os_click_react.js

---
+++ os/os_click_react.js
@@ -0,0 +1,4 @@
+function click() {
+  document.getElementById("os_html").style.property = new style.cursor = "url(load.cur)";
+  document.getElementById("os_button").style.property = new style.cursor = "url(load.cur)";
+}
