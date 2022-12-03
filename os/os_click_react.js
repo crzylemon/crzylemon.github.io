ea76923be553b2c4b9a66d9144b553694d4613cd
Crzymnkyboy <115381147+emmettdewet14@users.noreply.github.com>
2022-12-02 21:30:52 -0500

    Update os_click_react.js

--- os/os_click_react.js
+++ os/os_click_react.js
@@ -1,4 +1,4 @@
 function click() {
-  document.getElementById("os_html").style.cursor = new style.cursor = "url(load.cur)";
-  document.getElementById("os_button").style.cursor = new style.cursor = "url(clickload.cur)";
+  document.getElementById("os_html").style.cursor = "url('load.cur')";
+  document.getElementById("os_button").style.cursor = "url('clickload.cur')";
 }
