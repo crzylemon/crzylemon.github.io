c10ae9d2bb928a88204c0769fd967c8fcebac5f6
Crzymnkyboy <115381147+emmettdewet14@users.noreply.github.com>
2022-12-07 20:49:24 -0500

    such a big change

--- javascript.js
+++ javascript.js
@@ -1,52 +1,59 @@
-class javascript {
-    constructor() {
-    }
-    
-    getInfo() {
-        return {
-            "id": "js", //Extension Id
-            "name": "Javascript", //Extension name
-                        "blocks": [
-                        {
-                            "opcode": "js",
-                            "blockType": "command", //Block type - Command: Connections up and down, Hat: Event - Cap: Connections only up - Boolean: True or false - Reporter: A Value...
-                            "text": "Run code [input]",
-                            "arguments": {
-                                "input": {
-                                    "type": "string", //Put the type of menu - like: dropdown, string and number
-                                    "defaultValue": "message1" //Default Value
-                                }, {
-                            "opcode": "jsreport",
-                            "blockType": "reporter", //Block type - Command: Connections up and down, Hat: Event - Cap: Connections only up - Boolean: True or false - Reporter: A Value...
-                            "text": "Run code [input1]",
-                            "arguments": {
-                                "input1": {
-                                    "type": "string", //Put the type of menu - like: dropdown, string and number
-                                    "defaultValue": "message1" //Default Value
-                                },
-                            }
-                        },
-                ]
-        };
-    }
-    
-        js({input}) { //Block ID and menus
-        var F=new Function (input);
 
-        return(F());
 
-    },
-    
-            jsreport({input1}) { //Block ID and menus
-        var F1=new Function (input1);
+class js {
+	constructor() {}
 
-        return(F1());
+	getInfo() {
+		return {
+			"id": "javascript",
+			"name": "Javascript",
+			"blocks": [{
+				"opcode": "js",
+				"blockType": "command",
+				"text": "Run javascript [input]",
+				"arguments": {
+					"input": {
+						"type": "string",
+						"defaultValue": "placeholder"
+					},
+				}
+			}, {
+				"opcode": "jsreport",
+				"blockType": "reporter",
+				"text": "Run javascript [input1]",
+				"arguments": {
+					"input1": {
+						"type": "string",
+						"defaultValue": "placeholder"
+					},
+				}
+			}, {
+				"opcode": "jsbutton",
+				"blockType": "BlockType.BUTTON",
+				"text": "Run javascript [input2]",
+				"arguments": {
+				    "input2": {
+				        "type": "string",
+				        "defaultValue": "placeholder"
+				    }
+				},
+			}
+				  ]
+		};
+	}
 
-    }
-    
+	js({input}) {
+        eval(input);
+	}
+
+	jsreport({input1}) {
+		eval(input1);
+	}
+	
+	jsbutton({input2}) {
+		eval(input2);
+	}
+	
 }
 
-Scratch.extensions.register(new javascript());
-
-
-//extra tip for you guys, you can create new extensions like this
+Scratch.extensions.register(new js());
